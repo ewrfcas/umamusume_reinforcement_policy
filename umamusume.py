27e9f5cb5509084aff203aa588b967c896261284
caochenjie <ccjdurandal422@163.com>
2021-03-20 15:30:25 +0800

    commit

---
+++ umamusume.py
@@ -0,0 +1,11 @@
+class uma(object):
+    def __init__(self, name, states, feeling, skills, correction):
+        self.name = name
+        # states
+        self.states = states
+        self.feeling = feeling
+        self.skills = skills
+        self.correction = correction
+
+    
+
